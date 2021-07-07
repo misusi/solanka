0af3fa3196a849af17e91b37806a3719d183c1c9
Michael Susi <misusi@protonmail.com>
2021-07-07 15:40:20 -0500

    Beginning again... Sigh

--- src/com/synth/Synthesizer.java
+++ src/com/synth/Synthesizer.java
@@ -1,7 +1,39 @@
 package com.synth;
 
+import javafx.application.Application;
 import javafx.scene.layout.Pane;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
-public class Synthesizer {
+public class Synthesizer extends Application {
     Pane mainPane = new Pane();
+    final int SYNTH_WIDTH = 800;
+    final int SYNTH_HEIGHT = 500;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // Set up root window
+        Pane rootPane = new Pane();
+        rootPane.setMaxSize(SYNTH_WIDTH, SYNTH_HEIGHT);
+        rootPane.setMinSize(SYNTH_WIDTH, SYNTH_HEIGHT);
+        rootPane.setPrefSize(SYNTH_WIDTH, SYNTH_HEIGHT);
+
+        // Create oscillators
+
+        // Finalize stage/scene
+        Scene scene = new Scene(rootPane);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
